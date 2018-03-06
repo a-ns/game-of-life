b21bb516a548eb083a69da018ea181508aa5f53a
AleksanderS <exmann94@gmail.com>
2018-03-06 17:50:10 -0600

    moved automata logic to its own file

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component, Fragment } from "react";
 import styled from "styled-components";
 import Cell from "./Cell";
-
+import Automata from "./Automata";
 const Container = styled.div`
   width: ${props => props.cols * props.dim + "px"};
   height: ${props => props.rows * props.dim + "px"};
@@ -10,63 +10,15 @@ const Container = styled.div`
   grid-template-columns: ${props => `repeat(${props.cols}, 1fr)`};
   grid-template-rows: ${props => `repeat(${props.rows}, 1fr)`};
 `;
-function fillWithData(grid) {
-  for (var i = 0; i < grid.length; i++) {
-    for (var j = 0; j < grid[0].length; j++) {
-      grid[i][j] = Math.floor(Math.random() * Math.floor(2));
-    }
-  }
-  return grid;
-}
-
-function make2DArray(cols, rows) {
-  let arr = new Array(cols);
-  for (let i = 0; i < arr.length; i++) {
-    arr[i] = new Array(rows);
-  }
-  return arr;
-}
-
-function computeNeighbors(grid, x, y) {
-  let sum = 0;
-  for (var i = -1; i < 2; i++) {
-    for (var j = -1; j < 2; j++) {
-      sum +=
-        grid[(x + i + grid.length) % grid.length][
-          (y + j + grid[0].length) % grid[0].length
-        ];
-    }
-  }
-  sum -= grid[x][y];
-  return sum;
-}
-
-function computeNextState(state) {
-  //  0 -> 3 live -> 1
-  // 1 -> < 2 live || > 3 live -> 0
-  const { grid, next } = state;
-  for (var i = 0; i < grid.length; i++) {
-    for (var j = 0; j < grid[0].length; j++) {
-      let neighbors = computeNeighbors(grid, i, j);
-      if (neighbors === 3 && grid[i][j] === 0) {
-        next[i][j] = 1;
-      } else if (grid[i][j] === 1 && (neighbors < 2 || neighbors > 3)) {
-        next[i][j] = 0;
-      } else {
-        next[i][j] = grid[i][j];
-      }
-    }
-  }
-  return { grid: next, next: grid };
-}
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      grid: fillWithData(make2DArray(props.rows, props.cols)),
-      next: make2DArray(props.rows, props.cols)
+      automata: new Automata(props.rows, props.cols)
     };
+    this.state.grid = this.state.automata.grid;
+    this.state.nextGrid = this.state.automata.nextGrid;
   }
   componentWillReceiveProps(nextProps) {
     if (
@@ -74,19 +26,16 @@ class App extends Component {
       this.props.cols != nextProps.cols
     ) {
       this.setState({
-        grid: fillWithData(make2DArray(nextProps.cols, nextProps.rows)),
-        next: make2DArray(nextProps.cols, nextProps.rows)
+        automata: new Automata(nextProps.rows, nextProps.cols)
       });
     }
   }
   componentDidMount() {
     this.interval = setInterval(() => {
-      //const nextState = computeNextState(this.state)
-      //this.setState(nextState)
       this.setState(prevState => {
-        return computeNextState(prevState);
+        return this.state.automata.next();
       });
-    }, 1000 / this.props.fps);
+    }, this.props.fps);
   }
   componentWillUnmount() {
     clearInterval(this.interval);
---
+++ src/Automata.js
@@ -0,0 +1,62 @@
+class Automata {
+  constructor(rows, cols) {
+    this.rows = rows;
+    this.cols = cols;
+    this.grid = this.fillWithData(this.make2DArray(rows, cols));
+    this.nextGrid = this.make2DArray(rows, cols);
+  }
+  fillWithData = grid => {
+    for (var i = 0; i < grid.length; i++) {
+      for (var j = 0; j < grid[0].length; j++) {
+        grid[i][j] = Math.floor(Math.random() * Math.floor(2));
+      }
+    }
+    return grid;
+  };
+  make2DArray = (cols, rows) => {
+    let arr = new Array(cols);
+    for (let i = 0; i < arr.length; i++) {
+      arr[i] = new Array(rows);
+    }
+    return arr;
+  };
+  next() {
+    const { grid, nextGrid } = this.computeNextState(this);
+    this.grid = grid;
+    this.nextGrid = nextGrid;
+    return { grid, nextGrid };
+  }
+  computeNextState(state) {
+    //  0 -> 3 live -> 1
+    // 1 -> < 2 live || > 3 live -> 0
+    const { grid, nextGrid } = state;
+    for (var i = 0; i < grid.length; i++) {
+      for (var j = 0; j < grid[0].length; j++) {
+        let neighbors = this.computeNeighbors(grid, i, j);
+        if (neighbors === 3 && grid[i][j] === 0) {
+          nextGrid[i][j] = 1;
+        } else if (grid[i][j] === 1 && (neighbors < 2 || neighbors > 3)) {
+          nextGrid[i][j] = 0;
+        } else {
+          nextGrid[i][j] = grid[i][j];
+        }
+      }
+    }
+    return { grid: nextGrid, nextGrid: grid };
+  }
+  computeNeighbors(grid, x, y) {
+    let sum = 0;
+    for (var i = -1; i < 2; i++) {
+      for (var j = -1; j < 2; j++) {
+        sum +=
+          grid[(x + i + grid.length) % grid.length][
+            (y + j + grid[0].length) % grid[0].length
+          ];
+      }
+    }
+    sum -= grid[x][y];
+    return sum;
+  }
+}
+
+export default Automata;
